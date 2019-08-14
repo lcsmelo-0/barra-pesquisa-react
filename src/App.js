815ca50a6fd2edcead64a4fa4fc6ba9c33567515
Lucas Melo <luucasmell@gmail.com>
2019-08-13 23:35:20 -0300

    Adicionar navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component }from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './App.css'
+
+import logo from './assets/images/logo.svg'
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-
+        <div className="App">
+          <nav className="NavBar">
+            <a href="/">
+              <img className="logo" src={logo} alt="logo acesso" />
+            </a>
+          </nav>
+        </div>
       </div>
     )
   }
