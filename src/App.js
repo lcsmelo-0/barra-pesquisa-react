ffc32b348821bab5369abe84ae92cb7bde4f2bd0
Lucas Melo <luucasmell@gmail.com>
2019-08-13 23:38:48 -0300

    Importar dependencias do router para reinderizar componente home

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import React, { Component }from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './App.css'
 
 import logo from './assets/images/logo.svg'
 import user from './assets/images/user.png'
 import arrow from './assets/images/arrow.png'
+import Home from './Home/Home';
 
 class App extends Component{
   render(){
     return(
-      <div className="App">
+      <Router>
         <div className="App">
-          <nav className="NavBar">
-            <a href="/">
-              <img className="logo" src={logo} alt="logo acesso" />
-            </a>
+          <div className="App">
+            <nav className="NavBar">
+              <a href="/">
+                <img className="logo" src={logo} alt="logo acesso" />
+              </a>
 
-            {/* USER ACCESS */}
-            <div className="user-access">
-              <img src={user} alt="person icon" />
-              <p>John Due</p>
-              <img className="arrow" src={arrow} alt="arrow icon" />
-            </div>
-          </nav>
+              {/* USER ACCESS */}
+              <div className="user-access">
+                <img src={user} alt="person icon" />
+                <p>John Due</p>
+                <img className="arrow" src={arrow} alt="arrow icon" />
+              </div>
+            </nav>
+          </div>
+          <Route exact path="/" component={Home} />
         </div>
-      </div>
+      </Router>
     )
   }
 }
